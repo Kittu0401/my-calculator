ef6aa02387ded193f0b5d72b67a4d2e67262c2b3
Kittu0401 <136501642+Kittu0401@users.noreply.github.com>
2023-06-14 00:20:10 +0530

    Added 3 Files
    This is calculator project

---
+++ calculator/javascript.js
@@ -0,0 +1,52 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+
+
+        else if(e.target.innerHTML == '^2'){
+            var result = document.getElementById("result");
+            var currentValue = parseFloat(result.value);
+            var squaredValue = currentValue * currentValue;
+            result.value = squaredValue;
+        }
+
+
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
+
+// Function to square the current result value
+function square() {
+    var input = document.getElementById("inputBox");
+    var currentValue = parseFloat(input.value);
+    var squaredValue = currentValue * currentValue;
+    input.value = squaredValue;
+}
+
+// Function to clear the result value
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+
